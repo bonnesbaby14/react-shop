fd42b3fa624f7c86ae4ca3510f35436abdd44d8a
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-22 21:58:57 -0600

    añadir imagnes y configuracion en webpack

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/ProductItem.scss';
-
+import cart from "@icons/bt_add_to_cart.svg"
 const ProductItem = () => {
 	return (
 		<div className="ProductItem">
@@ -11,7 +11,7 @@ const ProductItem = () => {
 					<p>Bike</p>
 				</div>
 				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+					<img src={ cart} alt="" />
 				</figure>
 			</div>
 		</div>
