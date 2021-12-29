bd36801b3f2aeebfd25058c2201585ee81f49f9a
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-28 19:50:57 -0600

    cambios para el menu

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../styles/ProductItem.scss';
-import cart from "@icons/bt_add_to_cart.svg"
+import addToCart from "@icons/bt_add_to_cart.svg"
 const ProductItem = () => {
+
+	const [cart,setCart]=useState([]);
+
+	const handleCart = ()=>{
+		setCart([]);
+	}
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -10,8 +16,9 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src={ cart} alt="" />
+				<figure onClick={handleCart}>
+					<img src={addToCart} alt="" />
+			
 				</figure>
 			</div>
 		</div>
